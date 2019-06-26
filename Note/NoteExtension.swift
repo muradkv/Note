9a730ee156e0d2986f94b371e0ef211100eaf5a6
murad <muradkv@gmail.com>
2019-06-26 23:43:49 +0300

    Add extension for Note struct

---
+++ Note/NoteExtension.swift
@@ -0,0 +1,13 @@
+//
+//  NoteExtension.swift
+//  Note
+//
+//  Created by murad on 26/06/2019.
+//  Copyright © 2019 murad. All rights reserved.
+//
+
+import Foundation
+
+extension Note {
+    
+}
